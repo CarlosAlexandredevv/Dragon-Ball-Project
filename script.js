6ec392cf05c4638e86febac9846f05e942fdb884
Carlos Alexandre <143346006+CarlosAlexandredevv@users.noreply.github.com>
2024-02-03 16:57:02 -0300

    Upload

---
+++ script.js
@@ -0,0 +1,32 @@
+const botoes = document.querySelectorAll(".botao");
+const personagens = document.querySelectorAll(".personagem");
+
+botoes.forEach((botao, indice) => {
+    botao.addEventListener("click", () => {
+        desselecionarBotao();
+        desselecionarPersonagem();
+
+        botao.classList.add("selecionado");
+        personagens[indice].classList.add("selecionado");
+    });
+
+    botao.addEventListener("mouseover", () => {
+        if (!botao.classList.contains("selecionado")) {
+            botao.classList.add("hover");
+        }
+    });
+
+    botao.addEventListener("mouseout", () => {
+        botao.classList.remove("hover");
+    });
+});
+
+function desselecionarPersonagem() {
+    const personagemSelecionado = document.querySelector(".personagem.selecionado");
+    personagemSelecionado.classList.remove("selecionado");
+}
+
+function desselecionarBotao() {
+    const botaoSelecionado = document.querySelector(".botao.selecionado");
+    botaoSelecionado.classList.remove("selecionado");
+}
